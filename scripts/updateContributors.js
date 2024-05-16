05b27b4c9ad9e392f6b1997f974c5b8f1b4c3276
rabbittian2023 <rabbittian2023@163.com>
2024-05-16 15:16:41 +0800

    Update updateContributors.js

--- scripts/updateContributors.js
+++ scripts/updateContributors.js
@@ -20,38 +20,53 @@ const typeMap = {
 function updateContributors(username, type) {
   const content = fs.readFileSync('.all-contributorsrc', 'utf-8');
   const contributors = JSON.parse(content);
+  
   console.log('contributors: ', contributors);
   console.log('username', username);
+  console.log('type: ', type);
   // 检查用户是否已存在
   const exists = contributors.contributors.some((contributor) => contributor.login === username);
   if (!exists) {
     console.log(`Adding new contributor: ${username}`);
     const command = `npx all-contributors-cli add ${username} ${type}`;
-    execSync(command);
-    console.log('Contributor added successfully.');
+    console.log(`Running command: ${command}`);
+    execSync(command, { stdio: 'inherit' });
+    console.log('成功添加贡献者.');
     
     // Generate the contributors list after adding a new contributor
-    console.log('Generating contributors list...');
+    console.log('生成贡献者名单...');
     const generateCommand = 'npx all-contributors-cli generate';
-    execSync(generateCommand);
-    console.log('Contributors list updated.');
+    console.log(`Running command: ${generateCommand}`);
+    execSync(generateCommand, { stdio: 'inherit' });
+    console.log('贡献者名单已更新.');
   } else {
-    console.log('Contributor already exists, skipping...');
+    console.log('已存在贡献者，跳过...');
   }
 }
 
 function main() {
   const username = process.env.GITHUB_ACTOR;
-  console.log('fix: 111updateContribcutors.js - GITHUB_ACTOR :>> ', process.env.GITHUB_ACTOR);
-  console.log('username :>> ', username);
+  
+  console.log('fix: 111updateContribcutors.js - GITHUB_ACTOR :>> ', username);
+  if (!username) {
+    console.error('未定义 GITHUB_ACTOR。.');
+    process.exit(1);
+  }
+  
   const lastCommitMessage = execSync('git log -1 --pretty=%B').toString().trim();
   const commitType = lastCommitMessage.split(' ')[0];
   const contributionType = typeMap[commitType] || 'code';
-  console.log('Last commit message: ', lastCommitMessage);
-  console.log('Commit type: ', commitType);
-  console.log('Contribution type: ', contributionType);
   
   updateContributors(username, contributionType);
+  
+  // 检查 git status 以确认有变更
+  const changes = execSync('git status --porcelain').toString().trim();
+  console.log('Git changes:', changes);
+  if (changes) {
+    console.log('检测到更改，继续提交.');
+  } else {
+    console.log('未检测到更改，跳过提交.');
+  }
 }
 
 main();
